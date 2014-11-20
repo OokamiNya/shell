c342bdbf60054970b5687ab18aac3bb6d29d93be
Applesauce136 <harrison.chiu.136@gmail.com>
2014-11-20 13:34:42 -0500

    alpha: executes command line input

---
+++ 8/harrison_chiu/main.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "execute.h"
+
+#define IN_LEN 256
+
+int main()
+{
+  char *raw_in = (char *)malloc(IN_LEN * sizeof(char));
+  while (1)
+    {
+      int pid;
+      fgets(raw_in, IN_LEN, stdin);
+      pid = fork();
+      if (pid == 0)
+	execstr(raw_in);
+    }
+  free(raw_in);
+}
