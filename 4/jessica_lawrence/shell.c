705b874bcdd8b45ee4b8d33c9b588ff1174fbc65
OokamiNya <42chrono@gmail.com>
2014-11-24 10:53:08 -0500

    REKT LMAO

--- 4/jessica_lawrence/shell.c
+++ 4/jessica_lawrence/shell.c
@@ -40,7 +40,7 @@ void splitcmd(char* s, char** args) {
 
 void docmd(char** args) {
   execvp (args[0], args);
-  printf ("wrong\n");
+  printf ("LMFAO no such command ur a dum@$$ XDDDDDD :^)\n");
   exit(0);
 }
 
