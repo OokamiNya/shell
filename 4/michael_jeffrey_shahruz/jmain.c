f6f1523acec71dcc418417570b5353144e7265f6
Jeffrey He <jh1676@gmail.com>
2014-11-24 10:58:12 -0500

    changes

--- 4/michael_jeffrey_shahruz/jmain.c
+++ 4/michael_jeffrey_shahruz/jmain.c
@@ -6,7 +6,6 @@
 
 // Execs a function, parsing the input and running execvp
 int exec_line(char *input);
-
 void runs_command(char *scpy);
 
 int main() {
@@ -14,19 +13,21 @@ int main() {
   char s[1024];
   char *s2;
   char *s1;
-  char *scpy;
-  int i;
   char *commands[1024];
-  
   while(1) {
     printf("^_^: ");
     fgets(s,sizeof(s),stdin);
+    
+    int i=0;
+    for(;s[i]!='\n';i++){
+    }
+    s[i]=0;
 
-
-    scpy = malloc(1024);
+    char *scpy;
+    scpy=(char*)malloc(1024);
     strcpy(scpy,s);
-
-    // this part parses the input by ";" and puts each command in commands
+    
+     // this part parses the input by ";" and puts each command in commands
     s1 = s;
     for(i=0;s1;i++){
       s2 = strsep(&s1,";");
@@ -36,38 +37,17 @@ int main() {
 
     for(i=0;scpy;i++) {
       scpy = commands[i];
-      printf("Command(%d): %s\n",i,scpy);
+      if(!scpy) {
+	break;
+      }
       runs_command(scpy);
     }
+
     
   }
 }
 
-void runs_command(char *scpy) {
-  int f =fork();
-  char s[1024];
-  char *first_arg;
-  first_arg = strsep(&scpy," ");
-    printf("scpy: %s\n",scpy);
-    
-    if(strcmp("exit",first_arg) == 0) {
-      printf("exiting\n");
-      exit(0);
-    } else if (strcmp("cd",first_arg) == 0) {
-      printf("cding\n");
-    } else {
-   if(f == 0) {
-      exec_line(s);
-    }
-    else {
-      wait(NULL);
-    }
-
-    }
-}
 int exec_line(char *s) {
- 
-  
   char* s2;
   char *array[256];
   
@@ -85,4 +65,37 @@ int exec_line(char *s) {
   array[i]=0;
   execvp(array[0],array);
   return 0;
+}
+
+void runs_command(char *scpy) {
+  char s[1024];
+  char *first_arg;
+  strcpy(s,scpy);
+  
+  first_arg = strsep(&scpy," ");
+    
+  if(strcmp("exit",first_arg) == 0) {
+      printf("Exiting\n");
+      exit(0);
+    } 
+    else if (strcmp("cd",first_arg) == 0) {
+      chdir(scpy);
+      char direct[1024];
+      getcwd(direct,sizeof(direct));
+      printf("Current Directory: %s\n",direct);
+    } 
+    else {
+      
+      int f = fork();
+      if(f == 0) {
+	printf("execing : %s\n",s);
+	exec_line(s);
+       	exit(-1);
+      }
+      else {
+	wait(NULL);
+      }
+      
+    }
+
 }
