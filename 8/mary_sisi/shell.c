83f0b62bf9a3346b750d8317ef742124137aa1c5
Sisi Zimmerman <zimmerman.sisi@gmail.com>
2014-11-23 22:06:53 -0500

    .

--- 8/mary_sisi/shell.c
+++ 8/mary_sisi/shell.c
@@ -11,58 +11,71 @@
 //int contains(char **args, char keys);
 //int handle(char **args);
 
-/*
-still don't get why that whole returning thing isn't working. because now i put execute
- seperately in its own function to make main less clutteres and args will pass through 
-to another function but won't return from the parse function. I think you were on to somehting
-in class when you tried to change the return type of the parse function. But anyway, for not I
-tried mocing on from that extraordinarily annoying isssue
+void print_prompt();
+void parse(char ** a); //parses user input into an array of strings
+int contains(char **a);
+int execute(char **a); //runs exec
 
+/*
+semi colons work but print out the promt over and over for each  semicolons used
  */
 
 
-/*Ok, so the parse is sort of working. If you test it out, you can see that
-at the end of the pasre function i print it out and each element is correct
-but then when i try to print it in main it doesn't work
-So when the value is being returned is where it gets messed up somehow
- 
-*/
 
-//parse
-char** parse(){
+void parse(char ** args){
 
-    char s1[256];
-    fgets(s1, sizeof(s1), stdin);
-    s1[strlen(s1)-1]='\0';
+  //make char array, s1, to hold user input
+  char s1[256];
+  //put the user input in s1
+  fgets(s1, sizeof(s1), stdin);
+  //and get rid of the newline at the end
+  s1[strlen(s1)-1]='\0';
 
-    char * s = s1;
-    char ** args = (char**)malloc(sizeof(char *) * 64);//64?
-    char * temp = strsep(&s, " ");
+  //make a char pointer, s, to the beginning of s1, which will later be used to iterate through s1
+  char * s = s1;
+  //make a char pointer, temp, which will aid s in iterating through s1
+  char * temp = strsep(&s, " ");
 
-    int i=0;
-    while(temp){
-      args[i] = temp;    
-      temp = strsep(&s, " ");
-      i ++;   
+  //after using strsep:
+  //s now points to the first argument provided by the user, which is now be followed by a terminating null
+  //temp points to the remainder of the user input (starting at the second argument, if it exists, and terminating after the end of the user input)
+
+  //make an array of char pointers, args, which will hold the parsed values as separate entities
+  //char * args[64];
+  //malloc space for each element in args using a while loop
+  int i=0;
+  while(i<64){
+    args[i] = (char*)calloc(64,sizeof(char));
+    i++;
+  }
+  //resetting the counter variable
+  i=0;
+
+  //until temp hits the terminating null in s1...
+  while(temp){
+    
+    //as long as temp is pointing to a non-empty value...
+    if(strcmp(temp,"") != 0){
+      //copy value at temp into allocated space at args[i]
+      strcpy(args[i],temp);
+      i++;   
     }
-    args[i] = 0;
-    
-    /* testing that parsing works
-      i=0;
-      while(args[i]){
-      printf("args[%d]:  %s\t",i,args[i]);
-      i++;
-      }*/
-    
-    //printf("inside parse: %d\n",&args);
 
-    return args;
+    //(if there are multiple spaces between arguments, the if statement will prevent the extra spaces from being added to args)
+
+    //move to next argument
+    temp = strsep(&s," ");
+  }
+
+  //terminate args
+  args[i] = 0;
 
 }
 
+
 int contains(char** args){
 
-  //xprintf("IN CONTAINS\n");
+  //printf("IN CONTAINS\n");
 
   //  testing that parsing works
   int i=0;
@@ -77,10 +90,12 @@ int contains(char** args){
   return 0;
 }
 
-
-int redirection (char * source, char * dest){
-
-  return 0;
+void print_array(char ** args){
+  int i = 0;
+  while(args[i]){
+    printf("args[%d]:  %s\t",i,args[i]);
+    i++;
+  }
 }
 
 void print_prompt(){
@@ -92,27 +107,53 @@ void print_prompt(){
 
 int execute(char ** args){
   //RUNNING PARSED CODE
-  if (strcmp(args[0], "exit") == 0){
-    //run = 0;
-    printf("BYE!!!!\n");
-  }
-  else if  (strcmp(args[0], "cd") == 0){
-    chdir(args[1]);
-    print_prompt();
-  }
-  else{
-    int f = fork();
-    int status;
-    if( !f ){
-      execvp(args[0], args );
-      //everything else
-      //redirection
-    }else{
-      wait(&status);
+  if(contains(args) == 0){//regular input no ; < > |(
+    //printf("NO semicolon");
+    if (strcmp(args[0], "exit") == 0){
+      //run = 0;
+      printf("BYE!!!!\n");
+    }
+    else if  (strcmp(args[0], "cd") == 0){
+      chdir(args[1]);
       print_prompt();
     }
+    else{
+      int f = fork();
+      int status;
+      if( !f ){
+	execvp(args[0], args );
+	//everything else
+	//redirection
+      }else{
+	wait(&status);
+	print_prompt();
+      }
+    }
+  }else if (contains(args) == 1){
+    //printf("has semicolon!!\n");
+    char ** part1 =  (char**)malloc(sizeof(char *) * 64);
+    int j = 0; // counter for index of args
+    while( args[j]){   
+      if( strcmp(args[j], ";") != 0 ){
+	part1[j] = args[j];
+	j++;
+      }else{
+	execute(part1);
+	j++;
+	// this is super inefficent but it does work
+	char ** part2 =  (char**)malloc(sizeof(char *) * 64);
+	int i = 0;
+	while (args[j]){
+	  part2[i] = args[j];
+	  j++;
+	  i++;
+	}
+	execute(part2);	
+      }
+    }
   }
-  return 0;
+    return 0;
+  
 }
 
 int main(){
@@ -120,59 +161,11 @@ int main(){
   print_prompt();
   int run = 1;
   while(run){
-    
-    //PARSING
-    char s1[256];
-    fgets(s1, sizeof(s1), stdin);
-    s1[strlen(s1)-1]='\0';
-    
-    char * s = s1;
+
     char ** args = (char**)malloc(sizeof(char *) * 64);//64?
-    char * temp = strsep(&s, " ");
+    parse(args);
 
-    int i=0;
-    while(temp){
-      if(strcmp(temp,"") != 0){ // deals with aditional spaces issue
-	args[i] = temp;  
-	i ++;   
-      } 
-      temp = strsep(&s, " ");
-    }
-    args[i] = 0;
-    
-    /* i=0;
-    while(args[i]){
-      if(
-      printf("args[%d]:  %s\t",i,args[i]);
-      i++;
-      }*/
-
-    //execute(args);
-
-    if(contains(args) == 0){//regular input no ; < > |
-      execute(args);
-    }
-
-    //not exactly sure how i'm trying to do this, but this is the general idea of the ; part
-    /*else if(contains(args) == 1){//has ;
-      printf("has semicolon!!\n");
-      char ** part;
-      int j = 0; // counter for index of args
-      int k = 0;//counter for index of part
-      while( args[j]){
-	if( strcmp(args[j], ";") != 0 ){
-	  part[k] = args[j];
-	  k++;
-	}else{
-	  part[k] = 0;
-	  execute(part);
-	  int k = 0;
-	}
-	j++;
-      }
-      
-
-      }*/
+    execute(args);
     
   }
 
