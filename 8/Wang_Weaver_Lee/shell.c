02d737bf3883e38ab3f8df8768da28ace4b2242f
coweaver <cooperweaver@mac.com>
2014-11-24 14:06:54 -0500

    mycd

--- 8/Wang_Weaver_Lee/shell.c
+++ 8/Wang_Weaver_Lee/shell.c
@@ -3,33 +3,46 @@
 #include <string.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <errno.h>
 
 
 
 void parse(char *input);
-void mycd(){
-  printf("changed\n");
-}
-
-/*
-void mycd (char* arg) {
-  int err;
-  char *path = strsep(&arg, "\n");
-  char current [256];
-  getcwd (current, sizeof(current));
-  strcat(current, "/");
-  strcat(current, path);
-  err = chdir(current);
-  if (err=-1) {
-    printf("Error");
-  }
-}
- */
 
 
 
+void mycd (char** commands, int numargs) { 
+   char base[256];  
+   sprintf(base,"%s%s","/home/students/2015/",getenv("USER")); 
 
-void redirect(char *input){
+   char current[256]; 
+   getcwd(current, sizeof(current)); 
+
+   int errno; 
+   char path[256] = "";  
+
+   if(numargs == 1){ 
+     strcpy(path,base);
+
+   }else{
+     strcat(path, current);
+     strcat(path, "/"); 
+     strcat(path, commands[1]); 
+   }
+
+   
+   int err = chdir(path); 
+   if (err==-1) { 
+     printf("%s\n", strerror(errno)); 
+   } 
+ } 
+ 
+
+
+
+
+
+/*void redirect(char *input){
   if (strchr(input, '>')){
     char *p = strstr(input, "> ");
     int pend = strcspn(p + 2, " \n");
@@ -39,12 +52,15 @@ void redirect(char *input){
     int fdout = open(fileoutput, O_WRONLY |  O_CREAT | O_APPEND);
     dup(STDOUT_FILENO
   }
-  
+
   
   else{
     parse(input);
   }
 }
+
+
+*/  
 void parse(char* input){
  
   char **commands = (char **) calloc(64, sizeof(char *));
@@ -64,17 +80,17 @@ void parse(char* input){
   commands[numArgs]=0;
 
   if(strcmp(commands[0], "cd") == 0){
-    mycd(input);
+    mycd(commands, numArgs);
     for (i= 0; i < numArgs; i++){
-	free(commands[i]);
-      }
-      free(commands);
+      free(commands[i]);
+    }
+    free(commands);
   }
   else if(strcmp(commands[0], "exit") == 0){
     for (i= 0; i < numArgs; i++){
-	free(commands[i]);
-      }
-      free(commands);
+      free(commands[i]);
+    }
+    free(commands);
     exit(-1);
   }
 
@@ -101,15 +117,28 @@ int main(){
   while(1){
     char path[256];
     getcwd(path,sizeof(path));
-    //printf("%s\n", path);
+    printf("%s\n", path);
     char usr[256];
     strcpy(usr,getenv("USER"));
+    printf("%s\n", usr);
 
+   
     char *p = strstr(path,usr);
-    p += strlen(usr) + 1;
- 
+    printf("%s\n", usr);
+
+    if(p == NULL){
+     
+      printf("%s:%s ", usr, path);
+    }else{
+      printf("ahh");
+
+      p += strlen(usr) + 1;
+      // printf("%s\n", p);
     
-    printf("%s:~/%s$ ",usr,p);
+      printf("%s:~/%s$ ",usr,p);
+    }
+    *p = 0;
+
     char input[256];
     fgets(input,sizeof(input),stdin);
     char *tmp = strtok(input, ";");
@@ -124,7 +153,7 @@ int main(){
 
     int i;
     for(i = 0; i<numArgs; i++){
-      redirect(commands[i]);
+      parse(commands[i]);
     }
     for (i= 0; i < numArgs; i++){
       free(commands[i]);
