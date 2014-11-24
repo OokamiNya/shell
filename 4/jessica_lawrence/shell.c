eda6ff363c01b4612b8b09db41d2a07fb01e91e3
OokamiNya <42chrono@gmail.com>
2014-11-24 10:54:14 -0500

    fixed error :^)

--- 4/jessica_lawrence/shell.c
+++ 4/jessica_lawrence/shell.c
@@ -40,7 +40,7 @@ void splitcmd(char* s, char** args) {
 
 void docmd(char** args) {
   execvp (args[0], args);
-  printf ("LMFAO no such command ur a dum@$$ XDDDDDD :^)\n");
+  printf ("LMFAO no such command XDDDDDD q:^)-k\n");
   exit(0);
 }
 
