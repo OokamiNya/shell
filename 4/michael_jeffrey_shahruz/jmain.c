b289603762c4d8ec749ff6a857de284689709a5f
Jeffrey He <jh1676@gmail.com>
2014-11-24 16:30:40 -0500

    v

--- 4/michael_jeffrey_shahruz/jmain.c
+++ 4/michael_jeffrey_shahruz/jmain.c
@@ -7,44 +7,46 @@
 // Execs a function, parsing the input and running execvp
 int exec_line(char *input);
 void runs_command(char *scpy);
+void trim(char * str);
 
 int main() {
-  int status;
-  char s[1024];
-  char *s2;
-  char *s1;
-  char *commands[1024];
-  while(1) {
-    printf("^_^: ");
-    fgets(s,sizeof(s),stdin);
-    
-    int i=0;
-    for(;s[i]!='\n';i++){
-    }
-    s[i]=0;
+	int status;
+	char s[1024];
+	char *s2;
+	char *s1;
+	char *commands[1024];
+	while(1) {
+		printf("^_^: ");
+		fgets(s,sizeof(s),stdin);
 
-    char *scpy;
-    scpy=(char*)malloc(1024);
-    strcpy(scpy,s);
-    
+		int i=0;
+		for(;s[i]!='\n';i++){
+		}
+		s[i]=0;
+
+		char *scpy;
+		scpy=(char*)malloc(1024);
+		strcpy(scpy,s);
+
      // this part parses the input by ";" and puts each command in commands
-    s1 = s;
-    for(i=0;s1;i++){
-      s2 = strsep(&s1,";");
-      commands[i]=s2;
-    }
+		s1 = s;
+		for(i=0;s1;i++){
+			s2 = strsep(&s1,";");
+			commands[i]=s2;
+		}
     //
 
-    for(i=0;scpy;i++) {
-      scpy = commands[i];
-      if(!scpy) {
-	break;
-      }
-      runs_command(scpy);
-    }
+		for(i=0;scpy;i++) {
+			scpy = commands[i];
+			if(!scpy) {
+				break;
+			}
+			trim(scpy);
+			runs_command(scpy);
+		}
 
-    
-  }
+
+	}
 }
 
 int exec_line(char *s) {
@@ -98,4 +100,21 @@ void runs_command(char *scpy) {
       
     }
 
+}
+
+void trim(char *str) {
+	int i;
+	int begin = 0;
+	int end = strlen(str) - 1;
+
+	while(str[begin] == ' ') {
+		begin++;
+	}
+
+	while ((end >= begin) && str[end] == ' ')
+		end--;
+	for (i = begin; i <= end; i++) {
+		str[i - begin] = str[i];
+	}
+	str[i-begin] = '\0';
 }
