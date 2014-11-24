91558007f8eb3bcd9fb165b56d9b84d0af4095c2
Alex Libman <kingalex11235@yahoo.com>
2014-11-24 14:06:12 -0500

    redirection working in one direction

--- 8/alex_david/shell.c
+++ 8/alex_david/shell.c
@@ -44,10 +44,10 @@ int shell(){
   int n = 1;
   char *p = s;
   while (*p){ //now splits on ';' and runs commands in succession
-	if (*p == ';'){
-	  n++;
-	}
-	p++;
+    if (*p == ';'){
+      n++;
+    }
+    p++;
   }
   char **commands = malloc(sizeof(char *) * n);
   p = s;
@@ -55,12 +55,12 @@ int shell(){
   n = 0;
   char *k;
   while (k = strsep(&p,";")){
-	commands[n] = k;
-	n++;
+    commands[n] = k;
+    n++;
   }
   int i = 0;
   for (; i < n; i++){
-	execute(commands[i]);
+    execute(commands[i]);
   }
   free(commands);
   shell();
@@ -70,8 +70,8 @@ int execute(char *s){
   int n = 2;
   char *p = s;
   while (*p){
-	if (*p == ' '){
-	  n++;
+    if (*p == ' '){
+      n++;
     }
     p++;
   }
@@ -80,27 +80,39 @@ int execute(char *s){
   n = 0;
   char *k;
   while (k = strsep(&p," ")){
-	if (strcmp(k,"")){ //removes blanks from multiple spaces
-	  params[n] = k;
-	  n++;
-	}  
+    if (strcmp(k,"")){ //removes blanks from multiple spaces
+      params[n] = k;
+      n++;
+    }  
   }
   params[n] = NULL;
   if (!strcmp(params[0],"cd")){
-	int i = 1;
-	cd (params [i]); // note to self ~ and / don't work
-	//inputting just 'cd' causes a seg fault
+    int i = 1;
+    cd (params [i]); // note to self ~ and / don't work
+    //inputting just 'cd' causes a seg fault
   }else if (!strcmp(params[0],"exit")){
-	printf("Bye!\n\n");
-	exit(0);
+    printf("Bye!\n\n");
+    exit(0);
   }else{
-	int f = fork();
-	if (!f){
-	  execvp(params[0],params);    
-	}else{
-	  int status;
-	  wait(&status);
-	  free(params);
+    int f = fork();
+    if (!f){
+      int y = 0;
+      for (;y < n; y++){ //redirection
+	if (!strcmp(params[y],">")){
+	  int fd = open(params[y+1],O_CREAT | O_TRUNC | O_WRONLY,0644);
+	  dup2(fd,STDOUT_FILENO);
+	  params[y] = NULL;
+	}else if (!strcmp(params[y],">>")){
+	  int fd = open(params[y+1],O_CREAT | O_APPEND | O_WRONLY,0644);
+	  dup2(fd,STDOUT_FILENO);
+	  params[y] = NULL;
 	}
+      }
+      execvp(params[0],params);    
+    }else{
+      int status;
+      wait(&status);
+      free(params);
+    }
   }
 }
