2b10e2d250502b481ddb0e16c1cc2668a5e57f18
zhoubw <bz2118111@gmail.com>
2014-11-25 14:01:55 -0500

    restart...

--- 8/Barak_Zhou/oldshell.c
+++ 8/Barak_Zhou/oldshell.c
@@ -1,14 +1,9 @@
-/**
- * Shell.c
- * Contains main()
- *
- **/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 int execute(char* input) {
+  
   char** args = NULL;
   char* ptr = strtok(input, " ");
   int spaces = 0;
@@ -23,27 +18,20 @@ int execute(char* input) {
   args[spaces] = 0;
 
   execvp(args[0],args);
-  printf("done\n");
   return 0;
-  
-  
 }
+
 
 int main() {
   int running = 1;
   char input[256];
   while (running) {
-    printf("(╯'□')╯︵ ┻━┻ ");
-    fgets(input, sizeof(input), stdin);
-    int spoon = fork();
-    if (spoon < 0) {
-      printf("fork failed!\n");
-    }
-    else if (!spoon) {
-      execute(input);
-      exit(0);
-    }
-    printf("ended\n");
+    printf("$tacks on $tacks: ");
+    fgets(input,256,stdin);
+    //printf("%s", input);
+    execute(input);
+
   }
+  
   return 0;
 }
