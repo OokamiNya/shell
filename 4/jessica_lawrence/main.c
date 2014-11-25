bbe963b7061a743e4e90e5e9402a9fedf2e154ba
OokamiNya <42chrono@gmail.com>
2014-11-25 11:04:30 -0500

    ;;;;;;;;;;;;;;;;

--- 4/jessica_lawrence/main.c
+++ 4/jessica_lawrence/main.c
@@ -8,30 +8,41 @@
 int main () {
   char s[256];
   char **args = NULL;
+  char **cmds = NULL;
   int argc;
+  int cmdc;
+  int i;
   while (1) {
     prompt(s);
-    argc = countargs(s);
-    args = malloc((argc+1) * sizeof(char*));
-    splitcmd(s,args);
-    if (!strlen(s)) {
-      //no command given, do nothing
-    } else if(!strcmp(args[0],"exit")) {
-      gtfo();
-    } else if(!strcmp(args[0],"cd")) {
-      if (chdir (args[1])){
-        //will not take flags
-        printf ("no such directory GET FKIN NOSCOPED REKT LMAO\n");
-      }
-    } else {
-      int f = fork();
-      if(f == 0) {
-        docmd(args);
+    cmdc = countcmds(s);
+    cmds = malloc(cmdc * sizeof(char*));
+    splitinput(s,cmds);
+    i = 0;
+    while(i<cmdc) {
+      argc = countargs(cmds[i]);
+      args = malloc((argc+1) * sizeof(char*)); //+1 for NULL
+      splitcmd(cmds[i],args);
+      if (!strlen(cmds[i])) {
+        //no command given, do nothing
+      } else if(!strcmp(args[0],"exit")) {
+        gtfo();
+      } else if(!strcmp(args[0],"cd")) {
+        if (chdir (args[1])){
+          //will not take flags
+          printf ("no such directory GET FKIN NOSCOPED REKT LMAO\n");
+        }
       } else {
-        wait();
-      }
+        int f = fork();
+        if(f == 0) {
+          docmd(args);
+        } else {
+          wait();
+        }
+        }
+      i++;
     }
     free(args);
+    free(cmds);
   }
   return 0;
 }
