45a0c5f1af42eaf9b58641acd53f1fc7fc699fcd
sapphacs13 <sappha.omeara@gmail.com>
2014-11-26 11:45:48 -0500

    eh

--- 4/sappha_fawn_angela/shell.c
+++ 4/sappha_fawn_angela/shell.c
@@ -40,8 +40,6 @@ char** execute_all(){
 
   //deleting trailing newspace
   s1 = strsep(&s1, "\n");  
-
-  //check for pipes/redirection
   
   //parsing our command
   while (sep = strsep(&s1, ";")){
@@ -52,6 +50,9 @@ char** execute_all(){
     args[i] = temp;
     i++;
   }
+
+  //check for pipes/redirection
+  
   args = (char**)realloc(args, sizeof(char*)*(i));
   args[i] = NULL;
   return args;
