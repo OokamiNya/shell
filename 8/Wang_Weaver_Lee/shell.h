9b4e34a8af0725dd76b498d1a885b60979cd87ad
awang289@gmail.com <awang289@gmail.com>
2014-11-29 18:05:48 -0500

    added piping

---
+++ 8/Wang_Weaver_Lee/shell.h
@@ -0,0 +1,6 @@
+void parse(char *input);
+void mycd (char** commands, int numargs);
+void redirect(char *input);
+void process(char *input);
+void parse(char * input);
+void mypipe(char **commands, int numArgs);
