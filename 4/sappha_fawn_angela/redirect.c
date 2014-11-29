d5ec77ce9089f0b7f52ef572188c8bb8e3f2fa25
Angela Lin <angela97lin@gmail.com>
2014-11-29 16:34:35 -0500

    adding more files

--- 4/sappha_fawn_angela/redirect.c
+++ 4/sappha_fawn_angela/redirect.c
@@ -1,10 +1,14 @@
 #include <fcntl.h>
+#include <unistd.h>
 
 void redirect_stdout(char * from, char * to){
   //> 
   //STDOUT_FILENO
-  int fd;
+  int fd, fd1;
   fd = open(to, O_CREAT | O_TRUNC | O_WRONLY, 0644);
-  
+  fd1 = dup(STDOUT_FILENO);
+  dup2(fd, STDOUT_FILENO);
+  dup2(fd1, STDOUT_FILENO);
+
 }
 			  
