d7e906b4c697cb0e6a6b2f873078dab4823c595d
George Vorobyev <quaffle97@gmail.com>
2014-11-28 19:36:18 -0500

    updated headers

--- 4/george_vorobyev/util.h
+++ 4/george_vorobyev/util.h
@@ -15,10 +15,21 @@ typedef int redirect_mode;
 //puts each param into argv
 //returns argc, or -1 if error
 int argify(char *command, char *argv[]);
+
+//takes in one line of input, up to bytestoread long
+//discards up till \n if too long
 int _getline(char *in, int bytestoread);
+
+//does a particular redirect, including opening the file and 'dup2'ing
 int redirect(char *filename, redirect_mode mode);
+
+//does cd, returns -2 on success, -1 on error
 int gsh_cd(char *argv[]); //has to be NULL terminated
+
+//prints "$CWD>" and flushes output
 void print_prompt();
+
+//prints help message
 void print_help();
 
 #endif
