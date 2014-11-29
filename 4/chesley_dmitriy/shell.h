f0adeed7cd1141575d3bdfb89ac8da78e35e40b0
ChesleyTan <ChesleyTan97@gmail.com>
2014-11-29 11:03:51 -0500

    Better piping

--- 4/chesley_dmitriy/shell.h
+++ 4/chesley_dmitriy/shell.h
@@ -32,6 +32,7 @@
 #define MAX_CMD_SUBSTITUTION_SIZE 1024
 #define CMD_SUBSTITUTION_BUF_SIZE 512
 #define PIPE_TARGET_BUF_SIZE 512
+#define NO_FD -1
 
 // Shell built-in functions
 static const char *cmd_exit = "exit";
@@ -46,22 +47,29 @@ static const char STATE_CMD_SUBSTITUTION = 3;
 static const char STATE_REDIR_STDOUT_TO_FILE = 4;
 static const char STATE_REDIR_APPEND_STDOUT_TO_FILE = 5;
 static const char STATE_REDIR_FILE_TO_STDIN = 6;
-static const char STATE_PIPE = 7;
 
 // Parsing state terminating delimiters
 static const char *TERM_DELIM_STATE_REDIR_STDOUT_TO_FILE = " ;<|>\n";
 static const char *TERM_DELIM_STATE_REDIR_APPEND_STDOUT_TO_FILE = " ;<|>\n";
 static const char *TERM_DELIM_STATE_REDIR_FILE_TO_STDIN = " ;<|>\n";
-static const char *TERM_DELIM_STATE_PIPE = ";<|>\n";
 
 // Function type signatures
 static void sighandler(int signo);
 static void readline_sigint_handler();
 void print_error();
+int save_stdin();
+int save_stdout();
+int save_stderr();
+int save_default_fds();
+int restore_stdin();
+int restore_stdout();
+int restore_stderr();
+int restore_default_fds();
 void cd(const char *target);
 void cd_back();
 void abbreviate_home(char *full_path, size_t full_path_length);
 void execute();
+void reset_global_pipes();
 void reset_execute_variables();
 void free_all();
 void parse_input(char input[INPUT_BUF_SIZE]);
@@ -81,6 +89,5 @@ extern char debug_output;
 extern int cmd_nest_level;
 extern char *cmd_substitution_buffer;
 extern size_t cmd_substitution_buffer_index;
-extern char *pipe_target_buffer;
-extern size_t pipe_target_buffer_index;
+extern int global_pipes[2];
 
