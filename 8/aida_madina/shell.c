c812cd12f4385b3681412b15f06262a6948e48a3
Aida Piccato <aidapiccato@gmail.com>
2014-11-30 12:49:14 -0500

    Trying to do <

--- 8/aida_madina/shell.c
+++ 8/aida_madina/shell.c
@@ -22,7 +22,7 @@ int main() {
   FILE * fout = NULL;
   int exit_val = 105;
   int flag_redir_type = 0;
-
+  int file;
   
   while(1) {
     printf("seashell:%s$ ", getcwd(cwd, sizeof(cwd)));
@@ -47,67 +47,89 @@ int main() {
       char *comm = strtok(command, " ");
     
       if (!strcmp(comm,"exit")) {
-	exit(0);
+	      exit(0);
       }
     
       args_array[0] = comm;
       int j = 1;    
       if (num_args == 0) {
-	args_array[1]=NULL;
+	      args_array[1]=NULL;
       }
+
       else {
-	while (j <= num_args) {
-	  args_array[j] = strtok(NULL, " ");
-	  if (!strcmp(args_array[j], ">")) {
-	    flag_redir = j;
-	  }
-    else if (!strcmp(args_array[j],">>")) {
-      flag_redir_type = 1;
-      flag_redir = j;
-    }
-	  j++;
-	}  
-	args_array[j]=NULL;
+	      while (j <= num_args) {
+	        args_array[j] = strtok(NULL, " ");
+      	  if (!strcmp(args_array[j], ">")) {
+            flag_redir_type = 1;
+      	    flag_redir = j;
+    	    }
+          else if (!strcmp(args_array[j],">>")) {
+            flag_redir_type = 2;
+            flag_redir = j;
+          }
+          else if (!strcmp(args_array[j],"<")) {
+            flag_redir_type = 3;
+            flag_redir = j;
+          }
+      	  j++;
+	      }  
+	      args_array[j]=NULL;
       }
 
       if (!strcmp(comm,"cd")) {
         if (!args_array[1]) {
-	  chdir(getenv("HOME"));
+	         chdir(getenv("HOME"));
         }
         chdir(args_array[1]);
       }
 
       else if (flag_redir) {
-        int file;
-        if (flag_redir_type) {
-          file = open(args_array[flag_redir + 1], O_CREAT | O_WRONLY| O_APPEND, 0644);
+        if (flag_redir_type <= 2) {
+          if (flag_redir_type == 2) {
+            file = open(args_array[flag_redir + 1], O_CREAT | O_WRONLY| O_APPEND, 0644);
+          }
+          else if (flag_redir_type == 1) {
+            file = open(args_array[flag_redir + 1], O_CREAT | O_WRONLY| O_TRUNC, 0644);
+          }
+  	      pid = fork();
+  	      if (!pid) {
+        	  dup2(file,STDOUT_FILENO);
+        	  int n;
+        	  char *exec_args[10];
+        	  for(n = 0; n < flag_redir; n++) {
+        	    exec_args[n] = args_array[n];
+        	  }
+        	  exec_args[n] = NULL;
+        	  execvp(args_array[0], exec_args);
+        	  return 0;
+        	}
         }
         else {
-          file = open(args_array[flag_redir + 1], O_CREAT | O_WRONLY| O_TRUNC, 0644);
+          char *file_name = args_array[flag_redir + 1];
+          int input_file = open(file_name, O_RDONLY);
+          //char file_input[256];
+          //fgets(file_input, sizeof(file_input), input_file);
+          char *executable = args_array[flag_redir - 1];
+          printf("executable: %s\n", executable);
+          pid = fork();
+          if (!pid) {
+            dup2(STDIN_FILENO, input_file);
+            execlp(executable, executable, "testing testing", NULL);
+            return 0;
+          }
         }
-	     
-	pid = fork();
-	if (!pid) {
-	  dup2(file,STDOUT_FILENO);
-	  int n;
-	  char *exec_args[10];
-	  for(n = 0; n < flag_redir; n++) {
-	    exec_args[n] = args_array[n];
-	  }
-	  exec_args[n] = NULL;
-	  execvp(args_array[0], exec_args);
-	  return 0;
-	}
+
         flag_redir = 0;
+
       }
 
       else {
-	pid = fork();
+	      pid = fork();
         if(!pid) {
           execvp(args_array[0], args_array);	
           return WEXITSTATUS(exit_val);
         }
-	waitid(P_PID, pid, infop, WEXITED);
+      	waitid(P_PID, pid, infop, WEXITED);
       }
     }    
   }
