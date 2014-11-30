876ed0ca98dcd57123d429ce85a3ea6256f521ba
fawnwong <fawn.wong@live.com>
2014-11-29 23:16:23 -0500

    has_redirect

--- 4/sappha_fawn_angela/redirect.c
+++ 4/sappha_fawn_angela/redirect.c
@@ -1,21 +1,39 @@
 #include <fcntl.h>
 #include <unistd.h>
+#include <string.h>
 
 void redirect_in(char * from, char * to, int mode){
   //> redirecting stdout to a file
   //STDOUT_FILENO
-  int fd, fd1;
-  fd = open(to, O_CREAT | O_TRUNC | O_WRONLY, 0644);
-  fd1 = dup(STDOUT_FILENO);
-  dup2(fd, STDOUT_FILENO);
-  dup2(fd1, STDOUT_FILENO);
+	int fd, fd1;
+	fd = open(to, O_CREAT | O_TRUNC | O_WRONLY, 0644);
+	fd1 = dup(STDOUT_FILENO);
+	dup2(fd, STDOUT_FILENO);
+	dup2(fd1, STDOUT_FILENO);
 }
 
 
 //returns 1 if has redirect symbol (< , >)
+//returns 2 if has two redirect symbols right next to each other
 //else returns 0
 //how to handle double redirection			  
 int has_redirect(char * input){
-
-  return 0;
+	char * s = input;
+	int len = strlen(s);
+	int i;
+	for (i = 0; i < len; i++) {
+		if (s[i] == '<') {
+			if (s[i + 1] == '<')
+				return 2;
+			else
+				return 1;
+		}
+		if (s[i] == '>') {
+			if (s[i + 1] == '>')
+				return 2;
+			else
+				return 1;
+		}
+	}
+	return 0;
 }
