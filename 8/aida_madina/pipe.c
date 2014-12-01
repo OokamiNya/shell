b8ad2063775e566d19554632731536dca6b8d66c
apiccato <aidapiccato@gmail.com>
2014-12-01 13:56:03 -0500

    fixing

--- 8/aida_madina/pipe.c
+++ 8/aida_madina/pipe.c
@@ -1,9 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <signal.h>
-#include <fcntl.h>
 #include "pipe.h"
 
 int pipes(char **args, int index, int end) {
