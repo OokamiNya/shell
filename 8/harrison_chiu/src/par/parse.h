bed494fc455c7feaa5b96af190288402a329865f
Applesauce136 <harrison.chiu.136@gmail.com>
2014-12-01 14:03:56 -0500

    piping works!  Also, did the cd and exit things

--- 8/harrison_chiu/src/par/parse.h
+++ 8/harrison_chiu/src/par/parse.h
@@ -1,6 +1,7 @@
 #define DELIMS ";|"
 #define F_IN '<'
 #define F_OUT '>'
+#define TEMP_FILE "/tmp/MESS_STUFF"
 #define COMM_LEN 64
 
 typedef struct
@@ -8,6 +9,7 @@ typedef struct
   char *execstr;
   char *f_in;
   char *f_out;
+  int piped;
 } command_t;
 
 int parse(char **in, command_t **cmds, int num_cmds);
