0affc92785aa9192070640e959073afa4d95e602
CallMeSenpai <ericliao10797@hotmail.com>
2014-12-01 17:51:39 -0500

    lol

--- 4/[s]adman_[e]ric_[c]hristopher/shell.c
+++ 4/[s]adman_[e]ric_[c]hristopher/shell.c
@@ -29,6 +29,7 @@ int wrap_with_semicolons_LOL(char* stuff){
 //pipe accepts the whole line of args, 
 int doPipeStuff(char* arg){
   char* split_buffer=calloc(256,sizeof(char));
+  char* split_buffer_buffer=calloc(256,sizeof(char));
   char* arg_buffer=calloc(256,sizeof(char));
   
   int stdin_buffer=dup(STDIN_FILENO),
@@ -38,49 +39,63 @@ int doPipeStuff(char* arg){
     commands=countchar(arg,124)+1,/*124 is pipe*/
     command_index=0;
   for (;command_index<commands;command_index++){
+    char** addresses= calloc(256,sizeof(char*));
+    split_buffer=strsep(&arg,"|");
+    strcpy(split_buffer_buffer,split_buffer);
     int boolleft=countchar(split_buffer,'<');
     int boolright=countchar(split_buffer,'>');
-    split_buffer=strsep(&arg,"|");
     int i=0;
-    char** addresses= calloc(256,sizeof(char*));
-    while (arg_buffer=strsep(&split_buffer," "))
-      addresses[i++]=arg_buffer;
-    if (! fork()){
-      if(command_index){//if not at first command
-	file_in = open("piped",O_RDONLY);
-	dup2(file_in,STDIN_FILENO);
-	close(file_in);
-      }else if (boolleft==1){
-	printf("ran\n");
+    if (!boolleft && !boolright){
+      while (arg_buffer=strsep(&split_buffer," "))
+	addresses[i++]=arg_buffer;
+      i=0;
+    }
+    if ( fork()){
+      //reset stdin/out
+      close(file_in);
+      close(file_out);
+      dup2(stdin_buffer,STDIN_FILENO);
+      dup2(stdout_buffer,STDIN_FILENO);
+      wait(-1);
+    }else{
+      if (boolleft==1 && !command_index){
 	char* split_buffer2 = calloc(256,sizeof(char));
-	split_buffer2 = strsep(&split_buffer,"<");
-	split_buffer2 = strsep(&split_buffer,"<");
+	split_buffer2 = strsep(&split_buffer_buffer, ">"); 
+	while (arg_buffer=strsep(&split_buffer2," "))
+	  addresses[i++]=arg_buffer;
+      
+	split_buffer2 = strsep(&split_buffer_buffer, ">");
 	file_in = open(split_buffer2,O_RDONLY);
 	dup2(file_in,STDIN_FILENO);
 	close(file_in);
       }
-      if(commands-command_index-1){//if not at last command
-	file_out = open("piped",O_WRONLY|O_CREAT|O_TRUNC);
-	dup2(file_out,STDOUT_FILENO);
-	close(file_out);
-      }else if (boolright == 1){
-	printf("ran\n");
+      if (boolright == 1 && commands-command_index-1 == 0){
 	char* split_buffer2 = calloc(256,sizeof(char));
-	split_buffer2 = strsep(&split_buffer, ">"); 
-	split_buffer2 = strsep(&split_buffer, ">");
+	split_buffer2 = strsep(&split_buffer_buffer, ">");
+	while (arg_buffer=strsep(&split_buffer2," "))
+	  addresses[i++]=arg_buffer;
+	split_buffer2 = strsep(&split_buffer_buffer, ">");
 	//strsep for spaces
+	printf("into this file: %s\n",split_buffer2);
 	file_out = open(split_buffer2 , O_WRONLY|O_CREAT|O_TRUNC);
 	dup2(file_out,STDOUT_FILENO);
 	close(file_out);
       }
-      
+      if(command_index){//if not at first command
+	file_in = open("piped",O_RDONLY);
+	dup2(file_in,STDIN_FILENO);
+	close(file_in);
+	printf("open piped for command %s\n",addresses[0]);
+      }
+      if(commands-command_index-1){//if not at last command
+	file_out = open("piped",O_WRONLY|O_CREAT|O_TRUNC);
+	dup2(file_out,STDOUT_FILENO);
+	printf("wrote piped for command %s\n",addresses[0]);
+	close(file_out);
+      }
       execvp(addresses[0],addresses);
-    }else{
-      wait(-1);
-      //reset stdin/out
-      dup2(stdin_buffer,STDIN_FILENO);
-      dup2(stdout_buffer,STDIN_FILENO);
     }
+      
   }
   return 0;
 }
@@ -96,7 +111,7 @@ static void sighandler(int signo){
 
 int main(){
   signal(SIGINT, sighandler);
-  //printf("%d\n", getpid());
+  printf("shell PID:%d\n", getpid());
   umask(0000);
 
   char* args=calloc(256,sizeof(char));
@@ -112,7 +127,7 @@ int main(){
     char* p;
     for(p=args;*p;++p) *p= tolower(*p);
     //exit
-    if (! strncmp(args,"exit",4))
+    if (! strncmp(args,"exit",4) && getpid())
       exit(0);
     //cd
     else if (! strncmp(args,"cd",2)){
