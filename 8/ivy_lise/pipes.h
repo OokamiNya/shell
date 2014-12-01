ffc83759b460af862716ee2f6bf8dbb593c30ed4
koprty <koprty@users.noreply.github.com>
2014-11-30 19:37:35 -0500

    PIPING WORKS - separated pipe files

---
+++ 8/ivy_lise/pipes.h
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <pwd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <signal.h>
+#include <fcntl.h>
+#include "shell.h"
+
+void piper(char *);
